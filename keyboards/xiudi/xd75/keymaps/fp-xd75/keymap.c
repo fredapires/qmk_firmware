12dacf8d7e7f8b7f86a8bcd459ed507b140f5cfb
fredapires <fredapires@gmail.com>
2024-04-01 20:23:19 -0400

    updates to keymaps

--- keyboards/xiudi/xd75/keymaps/fp-xd75/keymap.c
+++ keyboards/xiudi/xd75/keymaps/fp-xd75/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [BASE] = { /* BASE */
-  { KC_GESC, 	KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    	KC_DEL,	 	KC_TRNS,  		KC_DEL,  	KC_6,    	KC_7,    KC_8,    KC_9,    KC_0,   	KC_TRNS },
+  { QK_GESC, 	KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    	KC_DEL,	 	KC_TRNS,  		KC_DEL,  	KC_6,    	KC_7,    KC_8,    KC_9,    KC_0,   	KC_TRNS },
   { KC_TAB,  	KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    	LSFT(KC_9),	KC_TRNS, 		LSFT(KC_0), KC_Y,    	KC_U,    KC_I,    KC_O,    KC_P,   	KC_TRNS },
   { KC_LCTL, 	KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    	KC_TRNS,	KC_TRNS,  		KC_TRNS, 	KC_H,    	KC_J,    KC_K,    KC_L,    KC_SCLN,	KC_ENT	},
   { KC_LSFT, 	KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    	KC_LBRC,	KC_TRNS, 		KC_RBRC, 	KC_N,    	KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT	},
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  },
 
 [RNAV] = { /* RNAV */
-  { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_F12,  KC_F6,   				KC_F7,   			KC_F8,   KC_F9,   		KC_F10,  		LALT(KC_F4)   	},
+  { QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_F12,  KC_F6,   				KC_F7,   			KC_F8,   KC_F9,   		KC_F10,  		LALT(KC_F4)   	},
   { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_BSLS, LCTL(LGUI(KC_LEFT)), 	KC_PGUP, 			KC_UP,   KC_PGDN, 		KC_TRNS, 		LGUI(KC_TAB)	},
   { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, 				KC_LEFT, 			KC_DOWN, KC_RGHT, 		KC_END,  		KC_RCTL  		},
   { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, SWIN(KC_LEFT), 		LCTL(LSFT(KC_TAB)), KC_TRNS, LCTL(KC_TAB), 	SWIN(KC_RGHT), 	KC_TRNS  		},
